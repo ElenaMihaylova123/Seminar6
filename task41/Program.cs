e86dd3e3b9f8f93d66f8a4e7425f2ca7a5406740
ElenaMihaylova <elena.16.04@mail.ru>
2023-06-01 18:50:27 +0700

    All done

---
+++ task41/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 41. Вводим с клавиатуры M чисел. Вывести количество чисел больше 0.
+
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0 : {count}");
